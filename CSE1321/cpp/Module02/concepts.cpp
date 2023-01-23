6bb896b4b3fc7dfcc0b67f4109126e72d4e22b79
eunsikim <kimeunsik229@gmail.com>
2023-01-23 09:04:01 -0500

    Uploaded CSE1321 C++ Module02 Concepts Blank

---
+++ CSE1321/cpp/Module02/concepts.cpp
@@ -0,0 +1,68 @@
+//  Concepts:
+//      Data Types
+//          Integers (int, short, long)
+//          Decimal Numbers (float & double)
+//          Boolean (bool)
+//          Characters (char) ''
+//          Strings (string) ""
+//      Constants
+//          #DEFINE 
+//          const
+//      Math Operations
+//          Addition +
+//          Subtraction -
+//          Multiplication *
+//          Division /
+//          Modulus %
+//      Casting
+//      Float/Double division vs Integer Division
+
+#include <iostream>
+
+using namespace std;
+
+//  Constants using DEFINE
+
+int main(){
+    //  Integer Example
+
+    
+    //  Short and Long (Just know they exist, usually we will use integer for most applications)
+
+
+    //  Float Example
+
+
+    //  Double Example
+
+
+    //  Boolean Example
+
+
+    //  Character Example
+
+
+    //  Strings Example
+
+
+    //  Constants
+
+
+    //  Math Operations
+
+
+    //  Modulus - Check if a number is even or odd
+
+
+    //  Modulus - Split a number
+
+
+    //  Casting
+
+
+    //  Float Division
+
+
+    //  Integer Division
+    
+}
